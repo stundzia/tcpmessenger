a64a922e73186fa20789fea7711466b2c10d6133
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-08-26 20:52:23 +0300

    More idiomatic forever loop (prevent main goroutine from exiting)

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"f.oxy.works/paulius.stundzia/tcpmessenger/messenger"
-	"time"
 )
 
 func main() {
 	msgr := messenger.GetMessenger(8033, 8044, 0)
 	msgr.Run()
-	for {
-		time.Sleep(2 * time.Second)
-	}
+	select {}
 }
