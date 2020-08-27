bce089586ae5fbbc39bc91a3bc03748ac858f1d3
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-08-27 21:49:01 +0300

    Add command line flags for consumer and producer ports

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"f.oxy.works/paulius.stundzia/tcpmessenger/messenger"
+	"flag"
 )
 
 func main() {
-	msgr := messenger.GetMessenger(8033, 8044)
+	producerPort := flag.Int("p", 8033, "Producer port (default: 8033)")
+	consumerPort := flag.Int("c", 8044, "Consumer port (default: 8044)")
+	flag.Parse()
+	msgr := messenger.GetMessenger(*producerPort, *consumerPort)
 	msgr.Run()
 	select {}
 }
