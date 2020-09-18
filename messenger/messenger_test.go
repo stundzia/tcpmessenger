35aecb8f0e34c109c66d405e7cdccb03dababa7c
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-09-18 14:30:32 +0300

    Add tests for `chat` mode

--- messenger/messenger_test.go
+++ messenger/messenger_test.go
@@ -28,7 +28,7 @@ func checkOutput(c net.Conn, expected []byte, t *testing.T) {
 	}
 }
 
-func setupAndTestConnection(port int, connType string, t *testing.T) net.Conn {
+func setupAndTestConnection(port int, connType string, name string, t *testing.T) net.Conn {
 	conn, err := net.Dial("tcp", fmt.Sprintf(":%d", port))
 	if err != nil || conn == nil {
 		t.Error("could not obtain connection: ", err)
@@ -37,23 +37,29 @@ func setupAndTestConnection(port int, connType string, t *testing.T) net.Conn {
 	if _, err := conn.Write([]byte(fmt.Sprintf("%s\n", connType))); err != nil {
 		t.Error("could not write payload to server: ", err)
 	}
-	var connTypeFull string
 	switch connType {
 	case "p":
-		connTypeFull = "producer"
+		checkOutput(conn, []byte("Entering `producer` mode\n"), t)
 	case "c":
-		connTypeFull = "consumer"
+		checkOutput(conn, []byte("Entering `consumer` mode\n"), t)
+	case "chat":
+		checkOutput(conn, []byte("Entering `chat` mode, enter your name:\n"), t)
+		if _, err := conn.Write([]byte(fmt.Sprintf("%s\n", name))); err != nil {
+			t.Error("could not write payload to server: ", err)
+		}
 	default:
 		t.Error("bad test case, unknown connType provided: ", connType)
 	}
-	modeMsg := fmt.Sprintf("Entering `%s` mode\n", connTypeFull)
-	checkOutput(conn, []byte(modeMsg), t)
 	return conn
+}
+
+func getExpectedChatMessage(name string, message []byte) []byte {
+	return []byte(fmt.Sprintf("%s: %s", name, message))
 }
 
 func TestMessenger_Run(t *testing.T) {
 	time.Sleep(1 * time.Second) // Give time for messenger to spin up.
-	setupAndTestConnection(8044, "c", t)
+	setupAndTestConnection(8044, "c", "", t)
 }
 
 func TestMessengerWithSingleProducerConsumerPair(t *testing.T) {
@@ -70,9 +76,11 @@ func TestMessengerWithSingleProducerConsumerPair(t *testing.T) {
 			[]byte("Where's the money, shithead?\n"),
 		},
 	}
+
 	time.Sleep(1 * time.Second)
-	producerConn := setupAndTestConnection(8044, "p", t)
-	consumerConn := setupAndTestConnection(8044, "c", t)
+
+	producerConn := setupAndTestConnection(8044, "p", "", t)
+	consumerConn := setupAndTestConnection(8044, "c", "", t)
 
 	defer producerConn.Close()
 	defer consumerConn.Close()
@@ -104,11 +112,14 @@ func TestMessengerWithMultipleProducersAndConsumers(t *testing.T) {
 			[]byte("You keep using that word. I do not think it means what you think it means.\n"),
 		},
 	}
-	producerConn := setupAndTestConnection(8044, "p", t)
-	producerConn2 := setupAndTestConnection(8044, "p", t)
-	consumerConn := setupAndTestConnection(8044, "c", t)
-	consumerConn2 := setupAndTestConnection(8044, "c", t)
-	consumerConn3 := setupAndTestConnection(8044, "c", t)
+
+	time.Sleep(1 * time.Second)
+
+	producerConn := setupAndTestConnection(8044, "p", "", t)
+	producerConn2 := setupAndTestConnection(8044, "p", "", t)
+	consumerConn := setupAndTestConnection(8044, "c", "", t)
+	consumerConn2 := setupAndTestConnection(8044, "c", "", t)
+	consumerConn3 := setupAndTestConnection(8044, "c", "", t)
 
 	defer producerConn.Close()
 	defer producerConn2.Close()
@@ -117,7 +128,6 @@ func TestMessengerWithMultipleProducersAndConsumers(t *testing.T) {
 	defer consumerConn3.Close()
 
 	consumerConns := []net.Conn{consumerConn, consumerConn2, consumerConn3}
-	time.Sleep(1 * time.Second)
 	for _, tc := range tcs {
 		t.Run(tc.test, func(t *testing.T) {
 			if _, err := producerConn.Write(tc.payload1); err != nil {
@@ -135,6 +145,53 @@ func TestMessengerWithMultipleProducersAndConsumers(t *testing.T) {
 			for _, cc := range consumerConns {
 				checkOutput(cc, tc.payload2, t)
 			}
+		})
+	}
+}
+
+func TestMessengerWithMultipleChatUsers(t *testing.T) {
+	tcs := []struct {
+		test     string
+		user1 string
+		payload1 []byte
+		user2 string
+		payload2 []byte
+	}{
+		{
+			"Sending a msg via any producer connection, sends the same message to all consumer connections",
+			"obi-van-kenobi",
+			[]byte("Hello there\n"),
+			"gen. grievious",
+			[]byte("General Kenobi\n"),
+		},
+		{
+			"Sending a msg via any producer connection, sends the same message to all consumer connections, part deux",
+			"InigoMontoya_nr1",
+			[]byte("Hello. My name is Inigo Montoya. You killed my father. Prepare to die.\n"),
+			"fingers_6",
+			[]byte("Huh?\n"),
+		},
+	}
+
+	time.Sleep(1 * time.Second)
+
+	for _, tc := range tcs {
+		t.Run(tc.test, func(t *testing.T) {
+			chatUser1 := setupAndTestConnection(8044, "chat", tc.user1, t)
+			chatUser2 := setupAndTestConnection(8044, "chat", tc.user2, t)
+			if _, err := chatUser1.Write(tc.payload1); err != nil {
+				t.Error("could not write payload to producer: ", err)
+			}
+
+			checkOutput(chatUser2, getExpectedChatMessage(tc.user1, tc.payload1), t)
+
+			if _, err := chatUser2.Write(tc.payload2); err != nil {
+				t.Error("could not write payload to producer: ", err)
+			}
+
+			checkOutput(chatUser1, getExpectedChatMessage(tc.user2, tc.payload2), t)
+			chatUser1.Close()
+			chatUser2.Close()
 		})
 	}
 }
